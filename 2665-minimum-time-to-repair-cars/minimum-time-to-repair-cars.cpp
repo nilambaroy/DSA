da7d9317d9f03a0814416968effc0ecf1357826b
Nilambar Roy <roynilambar268@gmail.com>
2025-03-24 18:21:16 +0530

    Time: 55 ms (41.78%) | Memory: 59.3 MB (78.75%) - LeetSync

---
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    #define ll long long
+    //nilambar
+    bool canAssign(vector<int>& ranks,ll& mid,int& cars){
+        ll count=0;
+        for(ll rank: ranks)
+            count += sqrt(mid/rank);
+        
+        return count>=cars;
+    }
+public:
+    long long repairCars(vector<int>& ranks, int cars) {
+        ll n = ranks.size();
+        ll low = 1;//For 1 car and 1 rank
+        ll high = (ll)*min_element(ranks.begin(),ranks.end()) * cars * cars;
+
+        ll ans;
+        while(low <= high){
+            ll mid = low + (high-low)/2;
+            if(canAssign(ranks,mid,cars)){
+                ans = mid;
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+};
